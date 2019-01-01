03a5dcacd133704b9542ec4167808c94531cd8d2
Markus Schwarz <post@markus-schwarz.net>
2019-01-01 17:43:06 +0100

    use different package and project name

---
+++ api/src/main/kotlin/de/codekenner/footballscoreboard/domain/Team.kt
@@ -0,0 +1,3 @@
+package de.codekenner.footballscoreboard.domain
+
+data class Team(val id: String, val name: String)
--- api/src/main/kotlin/de/codekenner/footballscoreboard/handler/GetTeamsHandler.kt
+++ api/src/main/kotlin/de/codekenner/footballscoreboard/handler/GetTeamsHandler.kt
@@ -1,21 +1,19 @@
-package com.serverless
+package de.codekenner.footballscoreboard.handler
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import de.codekenner.footballscoreboard.domain.Team
 import org.apache.logging.log4j.LogManager
 
-class Handler : RequestHandler<Map<String, Any>, ApiGatewayResponse> {
+class GetTeamsHandler : RequestHandler<Map<String, Any>, ApiGatewayResponse> {
     override fun handleRequest(input: Map<String, Any>, context: Context): ApiGatewayResponse {
         LOG.info("received: " + input.keys.toString())
-
         return ApiGatewayResponse.build {
-            statusCode = 200
-            objectBody = HelloResponse("Go Serverless v1.x! Your Kotlin function executed successfully!", input)
-            headers = mapOf("X-Powered-By" to "AWS Lambda & serverless")
+            objectBody = listOf(Team("foo", "bar"))
         }
     }
 
     companion object {
-        private val LOG = LogManager.getLogger(Handler::class.java)
+        private val LOG = LogManager.getLogger(GetTeamsHandler::class.java)
     }
 }
